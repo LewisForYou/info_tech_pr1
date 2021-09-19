9062045ed93a13458f7fa9cb2218f3791197c817
LewisForYou <74797340+LewisForYou@users.noreply.github.com>
2021-09-19 22:52:59 +0300

    add new method sort massive

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-/* Задача: посчитать максимальную площадь треугольника, если дан массив от 0 до 50
- Очевидно, что это будут три последние числа, тогда берём их */
+let arr = [49, 49, 48, -10, 8];
 
-let firstNum = 1
-let secondNum = 50
-
-Calc(firstNum, secondNum)
-function Calc(firstNum, secondNum){
-    if((secondNum || firstNum) < 0){ //проверяем больше ли нуля числа 
-        console.log(`Входные числа не могут быть меньше нуля!`);
+Calculate(arr)
+function Calculate(arr){
+    if (arr.length < 3){
+        console.log(`Длина массива меньше трёх числе!`)
     } else {
-        let arr = []
-        for(let i = firstNum; i <= secondNum; i++){
-        arr.push(i)
-        }
-        if (arr.length > 3){ // если больше трех чисел в массиве
-            let newArr = arr.slice(-3)
-            let a = newArr[0]
-            let b = newArr[1]
-            let c = newArr[2]
+        arr.length = 3
+        arr.sort((a, b) => b - a);
+        if ((arr[0] && arr[1] && arr[2]) > 0){
+            let a = arr[0]
+            let b = arr[1]
+            let c = arr[2]
             console.log(`Сторона 1 = ${a}, сторона 2 = ${b}, сторона 3 = ${c}`)
             let p = (a + b + c) / 2
             let Sum = Math.sqrt(p*(p-a)*(p-b)*(p-c))
             console.log(`Плоащль треугольника - ${Sum}`)
         } else {
-            console.log(`Длина масива минимум 3 числа, у вас - ${arr.length}`)
+            console.log(`В массиве имеется отрицательное число!`)
         }
     }
 }
